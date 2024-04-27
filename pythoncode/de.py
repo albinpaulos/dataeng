9b825f6a6826dd8e54b6eb702fdbf3ed308fc3d3
albinpaulos <47896907+albinpaulos@users.noreply.github.com>
2024-04-27 20:26:21 +0530

    Update de.py
    added vulnerable package

--- pythoncode/de.py
+++ pythoncode/de.py
@@ -3,6 +3,8 @@ import boto3
 from botocore.exceptions import NoCredentialsError
 from datetime import datetime
 import os
+import asyncio
+from aiohttp import web
 
 def extract_values_from_csv(csv_file):
     first_names = []
